3b69ed158c7f327b7a6ef11e7ef925091900c221
gluten111840 <bayueka980@gmail.com>
2020-07-21 22:11:35 +0700

    edit

--- project1/resources/views/user/index.blade.php
+++ project1/resources/views/user/index.blade.php
@@ -1,93 +1,65 @@
-@extends('layout.master')
-@section('title')
-Create Test
-@endsection
-@section('main')
-<div class="content">
-    <div class="module">
-        <div class="module-head">
-            <h3>
-                DataTables</h3>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Adding Question</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href=".">Quora</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                        <a class="nav-link" href=".">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="question">Question<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Answer</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Other
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Sign up new account</a>
+                            <a class="dropdown-item" href="#">Log out</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+        <div class="container"  style="text-align: center;">
+            <br>
+            <h1>Add Your Question Here!</h1>
+            <form action="{{ route('store') }}" method="POST">
+                @csrf
+                <h3 style="text-align: left;">Title</h3>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="title" placeholder="input your title here">
+                </div> 
+                <h3 style="text-align: left;">Question</h3>
+                <div class="form-group">
+                    <textarea class="form-control" name="question" rows="10" placeholder="input your question here"></textarea>
+                </div>
+                <input type="submit" class="btn btn-info" value="Submit">
+            </form>
+            <br>
+            <a href="question/detail" class="btn btn-warning" role="button">Show My Questions</a>
         </div>
-        <div class="module-body table">
-            <table cellpadding="0" cellspacing="0" border="0"
-                class="datatable-1 table table-bordered table-striped	 display" width="100%">
-                <thead>
-                    <tr>
-                        <th>
-                            Rendering engine
-                        </th>
-                        <th>
-                            Browser
-                        </th>
-                        <th>
-                            Platform(s)
-                        </th>
-                        <th>
-                            Engine version
-                        </th>
-                        <th>
-                            CSS grade
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="odd gradeX">
-                        <td>
-                            Trident
-                        </td>
-                        <td>
-                            Internet Explorer 4.0
-                        </td>
-                        <td>
-                            Win 95+
-                        </td>
-                        <td class="center">
-                            4
-                        </td>
-                        <td class="center">
-                            X
-                        </td>
-                    </tr>
-                    <tr class="even gradeC">
-                        <td>
-                            Trident
-                        </td>
-                        <td>
-                            Internet Explorer 5.0
-                        </td>
-                        <td>
-                            Win 95+
-                        </td>
-                        <td class="center">
-                            5
-                        </td>
-                        <td class="center">
-                            C
-                        </td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th>
-                            Rendering engine
-                        </th>
-                        <th>
-                            Browser
-                        </th>
-                        <th>
-                            Platform(s)
-                        </th>
-                        <th>
-                            Engine version
-                        </th>
-                        <th>
-                            CSS grade
-                        </th>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    </div>
-</div>
-@endsection
+
+
+    </body>
+</html>
